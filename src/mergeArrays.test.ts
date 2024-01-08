a34413967b86d92d86bbc770d636da9234adaab6
James <jizato.jirayu2001@gmail.com>
2024-01-08 17:19:09 +0700

    Update repo

---
+++ src/mergeArrays.test.ts
@@ -0,0 +1,9 @@
+import { merge } from './mergeArrays';
+
+describe('merge function', () => {
+  it('merges two sorted arrays', () => {
+    const result = merge([1, 3, 5], [2, 4, 6]);
+    expect(result).toEqual([1, 2, 3, 4, 5, 6]);
+  });
+});
+
---
+++ src/mergeArrays.ts
@@ -0,0 +1,31 @@
+// src/mergeArrays.ts
+
+export function merge(collection_1: number[], collection_2: number[]): number[] {
+    let result: number[] = [];
+    let i = 0;
+    let j = 0;
+  
+    while (i < collection_1.length && j < collection_2.length) {
+      if (collection_1[i] < collection_2[j]) {
+        result.push(collection_1[i]);
+        i++;
+      } else {
+        result.push(collection_2[j]);
+        j++;
+      }
+    }
+  
+    // Append the remaining elements, if any
+    while (i < collection_1.length) {
+      result.push(collection_1[i]);
+      i++;
+    }
+  
+    while (j < collection_2.length) {
+      result.push(collection_2[j]);
+      j++;
+    }
+  
+    return result;
+  }
+  
