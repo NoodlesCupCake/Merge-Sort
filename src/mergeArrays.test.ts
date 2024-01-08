8a77848629a97639d728d22dd477b15e18ebca73
James <jizato.jirayu2001@gmail.com>
2024-01-08 18:17:39 +0700

    Update the algorithm

--- src/mergeArrays.test.ts
+++ src/mergeArrays.test.ts
@@ -1,8 +1,8 @@
-import merge from './mergeArrays';
+import mergeSort from './mergeArrays';
 
 describe('merge function', () => {
   it('merges two sorted arrays', () => {
-    const result = merge([1, 3, 5], [2, 4, 6]);
+    const result = mergeSort([1, 3, 5], [2, 4, 6]);
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
   });
 });
--- src/mergeArrays.ts
+++ src/mergeArrays.ts
@@ -1,29 +1,34 @@
-function merge(collection_1: number[], collection_2: number[]): number[] {
-    let result: number[] = [];
-    let i = 0;
-    let j = 0;
-  
-    while (i < collection_1.length && j < collection_2.length) {
-      if (collection_1[i] < collection_2[j]) {
-        result.push(collection_1[i]);
-        i++;
-      } else {
-        result.push(collection_2[j]);
-        j++;
-      }
+function merge(leftArray: number[], rightArray: number[]): number[] {
+    let resultArray: number[] = [];
+    let leftIndex = 0, rightIndex = 0;
+
+    while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+        if (leftArray[leftIndex] < rightArray[rightIndex]) {
+            resultArray.push(leftArray[leftIndex]);
+            leftIndex++;
+        } else {
+            resultArray.push(rightArray[rightIndex]);
+            rightIndex++;
+        }
+    } 
+    
+    return [...resultArray, ...leftArray.slice(leftIndex), ...rightArray.slice(rightIndex)];
+}
+
+function mergeSort(collection_1: number[], collection_2: number[]): number[] {
+    if (collection_1.length <= 1 && collection_2.length <= 1) {
+        return merge(collection_1, collection_2);
     }
 
-    while (i < collection_1.length) {
-      result.push(collection_1[i]);
-      i++;
-    }
-  
-    while (j < collection_2.length) {
-      result.push(collection_2[j]);
-      j++;
-    }
-  
-    return result;
-  }
-  
-  export default merge;
+    const pivot_1 = Math.floor(collection_1.length / 2);
+    const leftArray_1 = collection_1.slice(0, pivot_1);
+    const rightArray_1 = collection_1.slice(pivot_1);
+
+    const pivot_2 = Math.floor(collection_2.length / 2);
+    const leftArray_2 = collection_2.slice(0, pivot_2);
+    const rightArray_2 = collection_2.slice(pivot_2);
+
+    return merge(mergeSort(leftArray_1, rightArray_1), mergeSort(leftArray_2, rightArray_2));
+}
+
+export default mergeSort;
