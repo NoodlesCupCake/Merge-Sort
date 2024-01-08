b6a11605eeb9e9e05f2ad1e407a8bbc7a31f87c2
James <jizato.jirayu2001@gmail.com>
2024-01-08 18:18:20 +0700

    Add unit test

--- src/mergeArrays.test.ts
+++ src/mergeArrays.test.ts
@@ -5,5 +5,10 @@ describe('merge function', () => {
     const result = mergeSort([1, 3, 5], [2, 4, 6]);
     expect(result).toEqual([1, 2, 3, 4, 5, 6]);
   });
+
+  it('merges two sorted arrays', () => {
+    const result = mergeSort([1, 3, 10], [2, 5, 9, 12]);
+    expect(result).toEqual([1, 2, 3, 5, 9, 10, 12]);
+  });
 });
 
